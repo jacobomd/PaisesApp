8cc856d80dc91e78765d22004b19a225ac6964df
Jacobo Morales <javoluis71@gmail.com>
2022-09-16 13:02:23 +0200

    creacion carpetas y comienzo de rutas

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { PorCapitalComponent } from './pais/pages/por-capital/por-capital.component';
+import { PorRegionComponent } from './pais/pages/por-region/por-region.component';
+import { VerPaisComponent } from './pais/pages/ver-pais/ver-pais.component';
+
+const routes: Routes = [
+  {path: '', component: PorCapitalComponent},
+  {path: 'region', component: PorRegionComponent},
+  {path: 'capital', component: PorCapitalComponent},
+  {path: 'pais/id', component: VerPaisComponent},
+  {path: '**', redirectTo: ''}
+]
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AppRoutingModule { }
